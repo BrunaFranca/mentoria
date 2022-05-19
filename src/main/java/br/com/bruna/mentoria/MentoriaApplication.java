b070e9e2de2725ea1c932b791d57ccb23691db7c
Bruna França <brunafranca@outlook.com>
2022-05-18 22:01:28 -0300

    Iniciando projeto java

---
+++ src/main/java/br/com/bruna/mentoria/MentoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruna.mentoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bruna/mentoria/MentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruna.mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
